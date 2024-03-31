42f79244f083f649c18d43e4a9fc19a2b04bb727
Venkata Shiva <venkatashiva4774@gmail.com>
2024-03-31 15:51:40 +0530

    Added Barcode Scanner to app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:lms/User_Pages/user_menu.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,8 +11,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  
+  String _scanResult = "";
   @override
+
+  Future<void> scanCode() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true, ScanMode.BARCODE);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to scan';
+    }
+
+    setState(() {
+      _scanResult = barcodeScanRes;
+    });
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -20,6 +36,7 @@ class _HomePageState extends State<HomePage> {
           IconButton(
             onPressed: () {
               // need to complete for bar-code scanner
+              scanCode();
             }, 
             icon: const Icon(Icons.qr_code_scanner, size: 24.0, color: Colors.black,)
           ),
